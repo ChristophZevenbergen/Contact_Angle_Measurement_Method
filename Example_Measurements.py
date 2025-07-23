cfa4c3a8ab6ff9a4f5ec1af1e6bdf64e9223844d
Christoph Zevenbergen <christophzevenbergen@gmail.com>
2025-07-23 16:31:46 -0300

    Add Example_measurements

---
+++ Example_Measurements.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 16 16:15:06 2025
+
+@author: Christoph Indiana Zevenbergen
+"""
+
+import sys
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+# Edit to folder of where the library file is located. 
+file_dir = '/home/usuario/Documentos/GitHub/Sphere Regression'
+sys.path.append(file_dir)
+import Lib_ContactAngle as esf
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+
+
+# Input file must be in the following format:
+# 0 - SOLID
+# 1 - FLUID 1
+# 2 - FLUID 2
+
+# Main function is: MeasureAngle
+# The code will measure the contact angle distribuition on the image using the sphere regression method and return the following:
+# A 2D NumPy array where each row corresponds to one detected contact point. Columns include:
+    # - [0]  Angle (degrees)
+    # - [1]  X coordinate of the contact point
+    # - [2]  Y coordinate of the contact point
+    # - [3]  Z coordinate of the contact point
+    # - [4]  Local pore diameter
+    # - [5]  Plane coefficient a (from ax + by + cz + d = 0)
+    # - [6]  Plane coefficient b
+    # - [7]  Plane coefficient c
+    # - [8]  Plane coefficient d
+    # - [9]  Sphere center X (C1)
+    # - [10] Sphere center Y (C2)
+    # - [11] Sphere center Z (C3)
+    # - [12] Sphere radius R
+    # - [13] Plane regression error (mean distance to plane)
+    # - [14] Sphere regression error (mean distance to sphere)
+    # - [15] Number of points for plane regression
+    # - [16] Number of points for sphere regression
+
+
+
+#Input file .raw unisigned char
+input_file = f'{file_dir}/Bentheimer_0125_A60.raw'
+
+#Size of the image
+size_x = 125
+size_y = 125
+size_z = 125
+
+# Subvolume Radius for Measurements
+# Default Values
+R1 = 5 # Plane regression subvolume radius
+alpha = 0.5 # Sphere regression subvbolume radius scaling factor
+
+
+#Read image as numpy three dimanesional array
+npimg = np.fromfile(input_file, dtype=np.uint8)
+imageSize = (size_x, size_y, size_z)
+npimg = npimg.reshape(imageSize)
+
+# Plot a slice of the porous media
+plt.imshow(npimg[:,:,50])
+
+
+# Main Function Measure the contact Angle
+Measurements = esf.MeasureAngle(npimg)
+
+
+Reference_measurements = np.load(f'{file_dir}/measurements.npy')
+
+if np.array_equal(Measurements, Reference_measurements):
+    print('Measurements taken successfully.')
+else:
+    print('Measurements do not match the reference. Please check your Python and library versions.')
